09194cec98053ee355360b18d4b2c487c13b545d
WNagorny <walery.nagorny@gmail.com>
2023-06-15 09:08:47 +0200

    added mobile navigation

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let cta_inputs = document.querySelectorAll('.form__input')
 let cta_btn = document.querySelector('.form__btn')
 
+const navBtn = document.querySelector('.nav-icon-btn')
+const navIcon = document.querySelector('.nav-icon')
+const topRow = document.querySelector('.header__top-row')
+
+
 const resetInputs = () => {
    cta_inputs.forEach(input => {
       input.value = "";
@@ -8,4 +13,15 @@ const resetInputs = () => {
 }
 
 
+
+
+const showMobileMenu = () => {
+   navIcon.classList.toggle('nav-icon--active')
+   topRow.classList.toggle('header__top-row--mobile')
+   document.body.classList.toggle('no-scroll')
+}
+
+
+
 cta_btn.addEventListener('click', resetInputs)
+navBtn.addEventListener('click', showMobileMenu)
