c4a6cb12d52130cc3bd1ee38e575006cc64a1824
WNagorny <walery.nagorny@gmail.com>
2023-06-23 15:55:53 +0200

    fixed burder menu navigation. Changed mobile view

--- script.js
+++ script.js
@@ -4,39 +4,38 @@ let cta_btn = document.querySelector('.form__btn')
 const navBtn = document.querySelector('.nav-icon-btn')
 const navIcon = document.querySelector('.nav-icon')
 const topRow = document.querySelector('.header__top-row')
-
+const ullist = document.querySelector('.nav__list')
 
 resetInputs = () => {
-   cta_inputs.forEach(input => {
-      input.value = "";
-   });
+	cta_inputs.forEach(input => {
+		input.value = ''
+	})
 }
-
 
 const showMobileMenu = () => {
-   navIcon.classList.toggle('nav-icon--active')
-   topRow.classList.toggle('header__top-row--mobile')
-   document.body.classList.toggle('no-scroll')
+	navIcon.classList.toggle('nav-icon--active')
+	topRow.classList.toggle('header__top-row--mobile')
 }
 
+const closeMobileMenu = () => {
+	topRow.classList.remove('header__top-row--mobile')
+	navIcon.classList.toggle('nav-icon--active')
+}
 
-
-
+ullist.addEventListener('click', closeMobileMenu)
 cta_btn.addEventListener('click', resetInputs)
 navBtn.addEventListener('click', showMobileMenu)
 
+document.addEventListener('DOMContentLoaded', function () {
+	const sr = ScrollReveal({
+		distance: '120px',
+		duration: 2500,
+		delay: 300,
+		reset: true,
+	})
 
-document.addEventListener('DOMContentLoaded', function() {
-   const sr = ScrollReveal({
-     distance: '120px',
-     duration: 2500,
-     delay: 300,
-     reset: true
-   });
- 
-   sr.reveal('.header__title', { delay: 200, origin: 'top' });
-   sr.reveal('.left', { delay: 400, origin: 'left' });
-   sr.reveal('.right', { delay: 400, origin: 'right' });
-   sr.reveal('.map', { delay: 400, origin: 'bottom' });
- });
-
+	sr.reveal('.header__title', { delay: 200, origin: 'top' })
+	sr.reveal('.left', { delay: 400, origin: 'left' })
+	sr.reveal('.right', { delay: 400, origin: 'right' })
+	sr.reveal('.map', { delay: 400, origin: 'bottom' })
+})
